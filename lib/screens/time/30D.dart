c27623c363658585b5cb338cfaa80e5d4160f4fd
szewah <swgrleong@gmail.com>
2020-10-13 12:32:12 -0400

    Tabview created

---
+++ lib/screens/time/30D.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThirtyDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Tab 30D Layout'),
+      ),
+    );
+  }
+}
